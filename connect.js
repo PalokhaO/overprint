07fa412bd37ffeef533d5175ba1bca7a0797125f
Oleksandr Palokha <palokha.oa@gmail.com>
2022-07-12 16:57:10 -0400

    serialport 2

--- connect.js
+++ connect.js
@@ -4,8 +4,10 @@ import * as readline from "readline";
 const port = new SerialPort({
     path: process.argv[2],
     baudRate: 115200,
-    endOnClose: false,
+    autoOpen: false,
 });
 
-const rl = readline.createInterface(port);
-rl.on("line", console.log);
+port.open(() => {
+    const rl = readline.createInterface(port);
+    rl.on("line", console.log);
+});
